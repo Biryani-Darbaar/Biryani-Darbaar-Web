88e79bdb84eed551781645cc7677924fad0829a4
Sarthak S Kumar <sarthakskumar7@gmail.com>
2026-07-28 13:04:07 +0530

    feat(contents): add Services implementation

--- src/contents/Services.ts
+++ src/contents/Services.ts
@@ -3,32 +3,32 @@ import { Service } from "@/types/common.types";
 export const services: Service[] = [
   {
     name: "Online Ordering",
-    image: "/assets/icons/spoon.png",
+    image: "/assets/icons/Online_Ordering.svg",
     alt: "Biryani Darbaar - Online Order",
   },
   {
     name: "24/7 Services",
-    image: "/assets/icons/247.png",
+    image: "/assets/icons/24_7Services.svg",
     alt: "Biryani Darbaar - 24/7 Services",
   },
   {
     name: "Pre-Reservations",
-    image: "/assets/icons/booking.png",
+    image: "/assets/icons/Pre-Reservations.svg",
     alt: "Biryani Darbaar - Pre-Reservation",
   },
   {
     name: "Organizing FoodHuts",
-    image: "/assets/icons/booking.png",
+    image: "/assets/icons/Organizing_FoodHuts.svg",
     alt: "Biryani Darbaar - Organized Place",
   },
   {
     name: "Super Chefs",
-    image: "/assets/icons/booking.png",
+    image: "/assets/icons/Super_Chefs.svg",
     alt: "Biryani Darbaar - Super Chef",
   },
   {
     name: "Clean Kitchens",
-    image: "/assets/icons/booking.png",
+    image: "/assets/icons/Clean_Kitchens.svg",
     alt: "Biryani Darbaar - Clean Kitchen",
   },
 ];
